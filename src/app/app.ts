99908fba194bb0be9233309308b2fe975fa179a2
Camila Espinosa <camilaespi1021@gmail.com>
2025-10-15 00:03:20 -0500

    ✨ [Feature] Crear componente Login con validaciones y simulación de autenticación

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './pages/login/login'; // ✅ este es tu login.ts
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, LoginComponent], // ✅ agregas el Login aquí
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
