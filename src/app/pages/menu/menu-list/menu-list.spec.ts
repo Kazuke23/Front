5dd457b6e0b0fdf1e8bb008e39c2f94c914a55f6
Camila Espinosa <camilaespi1021@gmail.com>
2025-11-17 14:33:16 -0500

    fix: Corregir archivo de pruebas menu-list.spec.ts - cambiar MenuList a MenuListComponent y agregar mocks

--- src/app/pages/menu/menu-list/menu-list.spec.ts
+++ src/app/pages/menu/menu-list/menu-list.spec.ts
@@ -1,18 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { ChangeDetectorRef } from '@angular/core';
+import { MenuService } from '../../../services/menu.service';
+import { AuthService } from '../../../services/auth.service';
 
-import { MenuList } from './menu-list';
+import { MenuListComponent } from './menu-list';
 
-describe('MenuList', () => {
-  let component: MenuList;
-  let fixture: ComponentFixture<MenuList>;
+describe('MenuListComponent', () => {
+  let component: MenuListComponent;
+  let fixture: ComponentFixture<MenuListComponent>;
+  let mockMenuService: jasmine.SpyObj<MenuService>;
+  let mockAuthService: jasmine.SpyObj<AuthService>;
+  let mockRouter: jasmine.SpyObj<Router>;
+  let mockCdr: jasmine.SpyObj<ChangeDetectorRef>;
 
   beforeEach(async () => {
+    mockMenuService = jasmine.createSpyObj('MenuService', ['getMenus', 'menus$']);
+    mockAuthService = jasmine.createSpyObj('AuthService', ['hasRole', 'authState$']);
+    mockRouter = jasmine.createSpyObj('Router', ['navigate', 'events']);
+    mockCdr = jasmine.createSpyObj('ChangeDetectorRef', ['detectChanges']);
+
+    mockMenuService.menus$ = jasmine.createSpyObj('Observable', ['subscribe']);
+    mockAuthService.authState$ = jasmine.createSpyObj('Observable', ['subscribe']);
+    mockMenuService.getMenus.and.returnValue([]);
+    mockAuthService.hasRole.and.returnValue(false);
+
     await TestBed.configureTestingModule({
-      imports: [MenuList]
+      imports: [MenuListComponent],
+      providers: [
+        { provide: MenuService, useValue: mockMenuService },
+        { provide: AuthService, useValue: mockAuthService },
+        { provide: Router, useValue: mockRouter },
+        { provide: ChangeDetectorRef, useValue: mockCdr }
+      ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(MenuList);
+    fixture = TestBed.createComponent(MenuListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
